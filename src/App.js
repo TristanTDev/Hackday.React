7a6e75b5fb79637cb898fe9ca158f5074fdcf336
Tristan <tristan@Tristans-MacBook-Pro.local>
2019-02-07 15:05:00 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MapComponent from './Map.component';
+import FilterComponent from './components/Filter.component'
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="border">
+          <div id="map-container">
+            <MapComponent />
+          </div>
+          <div id="filters-panel">
+            <FilterComponent />
+          </div>
+        </div>
       </div>
     );
   }
